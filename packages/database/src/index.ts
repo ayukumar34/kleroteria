3434440b5ef72741fd7c566c51a48cbab3a4da1f
Ayush <bigtexascompsci@proton.me>
2025-06-30 16:00:14 -0500

    feat: add Drizzle ORM integration and PostgreSQL schema
    - Introduced a new database package with Drizzle ORM for PostgreSQL
    - Configured database connection and schema definitions for users and sessions
    - Updated `pnpm-lock.yaml` to include new dependencies
    - Modified Express API to utilize the new database package

---
+++ packages/database/src/index.ts
@@ -0,0 +1,31 @@
+// Drizzle ORM
+import { drizzle } from "drizzle-orm/postgres-js";
+
+// PostgreSQL
+import postgres from "postgres";
+
+// Schema
+import {
+  users,
+  sessions,
+} from "./schema";
+
+const connectionString = process.env.SUPABASE_CONNECTION_STRING
+
+console.log(connectionString);
+
+if (!connectionString) {
+  throw new Error("SUPABASE_CONNECTION_STRING is not set in the environment variables");
+}
+
+// Create client
+const client = postgres(connectionString, { prepare: false });
+
+// Export schema
+export const schema = {
+  users,
+  sessions,
+};
+
+// Create database
+export const db = drizzle(client, { schema });
